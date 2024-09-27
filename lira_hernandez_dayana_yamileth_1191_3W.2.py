60eb5b7805471e75a113bbb02a3937411a3db12f
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-27 17:34:47 -0600

    Add files via upload

---
+++ lira_hernandez_dayana_yamileth_1191_3W.2.py
@@ -0,0 +1,21 @@
+print (" ") #espacio a agregar
+print ("Lira Hernandez Dayana Yamileth: programa que indica q numero es menor")
+print (" ") #espacio a agregar
+
+a = int(input("Ingrese primer valor"))  #instruccion para agregar numero
+b = int(input("Ingrese segundo valor"))  #instruccion para agregar numero
+if a < b: #if inicia funciones verdaderas o falsas
+    print ("El menor es:", a) #imprime si es verdad o falso
+elif b < a: #else indica si la funcion es falsa o verdadera
+    print ("El menor es:", b) #imprime si es verdad o falso
+print (" ") #espacio a agregar
+
+
+print (" ") #espacio a agregar
+print ("Suma de dos numeros") 
+print (" ") #espacio a agregar
+a = int(input("Ingresa un numero")) #instruccion para agregar numero
+b = int(input("Ingresa un numero")) #instruccion para agregar numero
+c = a + b #operacion a realizar
+print("El resultado es:", c) #imprime resultado de la operacion
+print (" ") #espacio a agregar
